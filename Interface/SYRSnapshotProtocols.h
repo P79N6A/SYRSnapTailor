155704fa5e98d04ff6c1cf0ae451add5c726def7
Jue Wang <wangjue.9489@bytedance.com>
2019-05-31 14:29:45 +0800

    [Modify] 初始化项目

---
+++ Interface/SYRSnapshotProtocols.h
@@ -0,0 +1,20 @@
+//
+//  SYRSnapshotProtocols.h
+//  SYRSnapTailorDemo
+//
+//  Created by 王珏 on 2019/5/31.
+//  Copyright © 2019 王珏. All rights reserved.
+//
+
+#ifndef SYRSnapshotProtocols_h
+#define SYRSnapshotProtocols_h
+
+@protocol SYRSnapshotDataSource <NSObject>
+
+@end
+
+@protocol SYRSnapshotDelegate <NSObject>
+
+@end
+
+#endif /* SYRSnapshotProtocols_h */
---
+++ SYRSnapTailorDemo/SYRSnapTailorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYRSnapTailorDemo
+//
+//  Created by 王珏 on 2019/5/31.
+//  Copyright © 2019 王珏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
